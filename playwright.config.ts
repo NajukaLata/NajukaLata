414306a18b9dca54faa7eda3c3134c034127b4b9
NajukaLata <rodenajuka2@gmail.com>
2024-08-04 06:39:58 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,15 @@ export default defineConfig({
   projects: [
     {
     //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
+    //   use: {devices['Desktop Firefox'] },
     // },
     // {
     //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   use: { devices['Desktop Safari'] },
     // },
     {
       name: 'google chrome',
-      use: { ...devices['Desktop Chrome'] },
+      use: { devices['Desktop Chrome'] },
     },
   ],
 });
